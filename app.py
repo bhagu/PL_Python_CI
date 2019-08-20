84cd13b4193dd6bffee86746971559a5c6a5d5ca
Bhagyaraj Chitoth <bhagyaraj.chitoth@anheuser-busch.com>
2019-08-20 19:37:05 +0530

    testing travis cicd option using the feature branch

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 def my_function(param1, param2):
     return param1 + param2
     
-# This is a comment to test the auto build with travis set up    
+# This is a comment to test the auto build with travis set up
+# This is the second comment to test the auto build with travis set up using a new branch 
