29d1be4ed6ef5164daee140e1cc3648da5f593e5
Bhagyaraj Chitoth <bhagyaraj.chitoth@anheuser-busch.com>
2019-08-20 19:30:24 +0530

    testing travis cicd option

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import app
 
 class MyTest(unittest.TestCase):
     def test_my_function(self):
-        self.assertEqual(my_function(1, 1), 2)
-        self.assertEqual(my_function(1, -1), 0)
-        self.assertEqual(my_function(0, 0), 0)
-        self.assertEqual(my_function(-1, -1), -2)
-        self.assertEqual(my_function(1.0, 1), 2)        
-        self.assertEqual(my_function(1.1, 1.1), 2.2)        
+        self.assertEqual(app.my_function(1, 1), 2)
+        self.assertEqual(app.my_function(1, -1), 0)
+        self.assertEqual(app.my_function(0, 0), 0)
+        self.assertEqual(app.my_function(-1, -1), -2)
+        self.assertEqual(app.my_function(1.0, 1), 2)        
+        self.assertEqual(app.my_function(1.1, 1.1), 2.2)        
         
         
         
